6f92a453844c2b7486e8082aa42dae02d319130f
Berkaytlli <hbtelli7@gmail.com>
2022-05-10 20:02:53 +0300

    Add project files.

---
+++ E-commerce Site/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using E_commerce_Site.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_commerce_Site.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
